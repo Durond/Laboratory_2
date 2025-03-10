a11d9602bbf8e8c35d957258c9290dc56c9725c9
Durond <lunkovdanila2003@mail.ru>
2025-03-10 15:34:50 +0800

    Добавьте файлы проекта.

---
+++ Laboratory_2/Laboratory_2.cpp
@@ -0,0 +1,59 @@
+﻿#define GLEW_DLL
+#define GLFW_DLL
+
+#include <iostream>
+#include "GL/glew.h"
+#include "GL/glfw3.h"
+
+
+int main()
+{
+    if (!glfwInit()) {
+        fprintf(stderr, "ERROR: could not start GLFW3.\n");
+        return 1;
+    }
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 1);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+    //glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_FALSE);
+    //glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow* window = glfwCreateWindow(512, 512, "MainWindow", NULL, NULL);
+    if (!window) {
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    GLenum ret = glewInit();
+    if (GLEW_OK != ret) {
+        fprintf(stderr, "Error: %s\n", glewGetErrorString(ret));
+        return 1;
+    }
+
+    glClearColor(1.0, 1.0, 0.5, 1.0);
+
+    while (!glfwWindowShouldClose(window)) {
+
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glBegin(GL_POLYGON);
+
+        glColor3f(0.2, 0.4, 1.0);
+        glVertex2f(1.0, 0.0);
+        glVertex2f(0.5, 0.866);
+        glVertex2f(-0.5, 0.866);
+        glVertex2f(-1.0, 0.0);
+        glVertex2f(-0.5, -0.866);
+        glVertex2f(0.5, -0.866);
+
+        glEnd();
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
+}
